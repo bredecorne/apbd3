977f4bc33aee93f09e0350153faacf9d24133406
Jakub Kowalewski <kowalewskijakub@icloud.com>
2024-04-14 18:37:16 +0200

    Implemented AnimalsController.GetAnimals()

---
+++ WebApplication/Controllers/AnimalsController.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using WebApplication.DTOs;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers;
+
+[ApiController]
+[Route("/api/animals")]
+public class AnimalsController : ControllerBase
+{
+    private IConfiguration _configuration;
+
+    public AnimalsController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public ActionResult<IEnumerable<AnimalDTO>> GetAnimals()
+    {
+        var sqlConnection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        var sqlCommand = new SqlCommand();
+        
+        sqlCommand.Connection = sqlConnection;
+        sqlCommand.CommandText = "SELECT * FROM Animals";
+        
+        sqlConnection.Open();
+
+        var sqlData = sqlCommand.ExecuteReader();
+        var animals = new List<Animal>();
+
+        while (sqlData.Read())
+        {
+            animals.Add(new Animal(
+                (int) sqlData["id"],
+                (string) sqlData["name"],
+                (string) sqlData["description"],
+                (string) sqlData["category"],
+                (string) sqlData["area"])
+            );
+        }
+        
+        return Ok();
+    }
+}
---
+++ WebApplication/Models/Animal.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication.Models;
+
+public class Animal
+{
+    private int Id { get; set; }
+    private string Name { get; set; }
+    private string? Description { get; set; }
+    private string Category { get; set; }
+    private string Area { get; set; }
+
+    public Animal(int id = default, string name = null, string? description = null, string category = null, string area = null)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        Category = category;
+        Area = area;
+    }
+}
